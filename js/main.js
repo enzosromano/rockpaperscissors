95e2c759abe7c9535e5c87cfabe0798da4aebd2e
Enzo Romano <enzosromano@gmail.com>
2022-06-16 10:47:33 -0400

    RPS logic finalized and printed to console Changed the logic to determine the winner Changed the logic to determine what hand the computer plays

--- js/main.js
+++ js/main.js
@@ -1,29 +1,30 @@
 function computerPlay(){
-    let choice = Math.floor(Math.random() * 3);
-    if (choice == 0) {
-        console.log("rock");
-        return("Rock");
-    }
-    else if (choice == 1) {
-        console.log("paper");
-        return("Paper");
-    }
-    else {
-        console.log("scissors")
-        return("Scissors");
-    }
+
+    return(Math.floor(Math.random() * 3)); 
+
 }
 
 function determineWinner(playerSelection, computerSelection){
-    if (input == output){
-        console.log("Its a tie")
+
+    if(((playerSelection + 1) % 3) == computerSelection) {
+        return("Computer won");
+    }
+    else if(playerSelection == computerSelection) {
+        return("It's a tie");
     }
     else {
-        console.log("Somebody lost. You chose: " + input + ", and they chose " + output);
+        return("You won!");
     }
 }
 
+function game() {
+    
+}
+
 let output = computerPlay();
-let input = "Paper";
-determineWinner(input, output);
+let input = Math.floor(Math.random() * 3);
+console.log("0:R   1:P   2:S")
+console.log("You chose: " + input);
+console.log("Computer Chose: " + output);
+console.log(determineWinner(input, output));
   
