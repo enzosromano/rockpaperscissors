a713a128762bc13c814bef031645b5d8b0d3a297
Enzo Romano <enzosromano@gmail.com>
2022-06-16 11:08:02 -0400

    Added the "game" function The game function simulates multiple rounds of RPS Added switch statement to determine logic of who gets points

--- js/main.js
+++ js/main.js
@@ -7,18 +7,42 @@ function computerPlay(){
 function determineWinner(playerSelection, computerSelection){
 
     if(((playerSelection + 1) % 3) == computerSelection) {
-        return("Computer won");
+        /* Computer Won */
+        return(1);
     }
     else if(playerSelection == computerSelection) {
-        return("It's a tie");
+        /* Tie */
+        return(0);
     }
     else {
-        return("You won!");
+        /* You won */
+        return(2);
     }
 }
 
 function game() {
-    
+
+    let playerWins = 0;
+    let computerwins = 0
+
+    while((playerWins < 5) && (computerwins < 5)){
+
+        let winner = determineWinner(0, computerPlay());
+
+        switch(winner) {
+            case 0:
+                console.log("Its a tie")
+                break;
+            case 1:
+                computerwins++;
+                console.log("Computer Won. Computer has " + computerwins + " wins.");
+                break;
+            case 2:
+                playerWins++;
+                console.log("You Won. You have " + playerWins + " wins.");
+                break;
+        }
+    }
 }
 
 let output = computerPlay();
@@ -27,4 +51,5 @@ console.log("0:R   1:P   2:S")
 console.log("You chose: " + input);
 console.log("Computer Chose: " + output);
 console.log(determineWinner(input, output));
+game();
   
